4474f0bbf4b3d5ae46fbf315efd8d11508827538
김다은 <softeun92@naver.com>
2021-11-13 15:21:24 +0900

    메인화면으로 보낸 문구를 표시할 자리

---
+++ app/src/main/java/com/nepplus/intent_20211113/ActivityToother.kt
@@ -0,0 +1,22 @@
+package com.nepplus.intent_20211113
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_toother.*
+
+class ActivityToother : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toother)
+
+        btnRetrunMain.setOnClickListener {
+
+            val myintent = Intent(this,MainActivity::class.java)
+
+            finish()
+//            startActivity(myintent)
+
+        }
+    }
+}
