1ddce21e5c51daf680e90302ae74382e5285914a
김다은 <softeun92@naver.com>
2021-11-13 16:22:09 +0900

    변경확인
    입력값변수저장
    결과데이터 첨부용 intent / putEXtra
    RESULT_OK,resultIntent
    
    finish()

--- app/src/main/java/com/nepplus/intent_20211113/EditNicnameActivity.kt
+++ app/src/main/java/com/nepplus/intent_20211113/EditNicnameActivity.kt
@@ -1,11 +1,25 @@
 package com.nepplus.intent_20211113
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nicname.*
 
 class EditNicnameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nicname)
+
+
+        bntNickName.setOnClickListener {
+
+            val inputNickname = entNickName.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+            setResult(RESULT_OK,resultIntent)
+
+            finish()
+        }
     }
 }
