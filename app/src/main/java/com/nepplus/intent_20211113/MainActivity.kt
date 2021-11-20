92a265f4b93a2abb590b79a7703db63022d80c38
윤성노 <softeun92@naver.com>
2021-11-20 11:21:08 +0900

    web체험

--- app/src/main/java/com/nepplus/intent_20211113/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_20211113/MainActivity.kt
@@ -13,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnNaverWebLink.setOnClickListener {
+            val myUri = Uri.parse("https://www.naver.com/")
+            val myIntent = Intent(Intent.ACTION_VIEW,myUri)
+            startActivity(myIntent)
+        }
+
         btnSms.setOnClickListener {
             val inputPhoneNum = edtPhoneNum.text.toString()
 //            그 전화번호에 실제 전화연결
